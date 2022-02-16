25f8eabb9183641e106edbe6895bc3a8871ee9b7
Waldo Lavaut <waldothedeveloper@gmail.com>
2022-02-16 09:26:56 -0500

    Initial setup project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,58 @@
+const path = require("path");
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  "node_modules",
+  "gatsby",
+  "dist",
+  "utils",
+  "eslint-rules"
+);
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-      title: `cubason`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `cubason`,
+    siteUrl: `https://www.cubason.co`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "y6-BXzDl_pc291y9vPo_OTCd-vQdi3B3x71W6TZHJgo",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        rulePaths: [gatsbyRequiredRules],
+        stages: ["develop", "build-javascript"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"],
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESSTOKEN,
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACEID,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
